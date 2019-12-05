bb85512b3e34e6de8751f1990b15d5e8ea1cfcb0
barraud-samuel <samuel.barraud@hetic.net>
2019-12-05 11:52:30 +0100

    inital commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate";
+import EventList from "../views/EventList";
+import EventShow from "../views/EventShow";
+import NotFound from "../views/NotFound";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/event/:id",
+    name: "event-show",
+    component: EventShow,
+    props: true
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
+  },
+  {
+    path: "*",
+    component: NotFound
   }
 ];
 
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Create Listing</h1>
+</template>
+
+<script>
+export default {
+  name: "EventCreate"
+};
+</script>
+
+<style scoped></style>
